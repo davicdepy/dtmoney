87c5ab23c73f6ac9469e7c6e485b6ba0988dbc78
David Gómez <davidcdepy@gmail.com>
2021-04-26 01:47:00 -0400

    Ignite-Trilha-ReactJS-ChapterII-Componente:TransactionsTable

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     :root {
+        
         --red: #E52E4D;
         --blue: #5429CC;
         --green: #33CC95;
